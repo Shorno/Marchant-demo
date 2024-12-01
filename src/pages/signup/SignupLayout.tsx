6d9f84ff7bac3816610f03f0dbcf7a340acc8fbb
Shorno <38567217+Shorno@users.noreply.github.com>
2024-12-01 10:35:38 +0600

    fix/signup-design (#18)
    * fix marge issue
    
    * fix layout

--- src/pages/signup/SignupLayout.tsx
+++ src/pages/signup/SignupLayout.tsx
@@ -1,33 +1,30 @@
-import React, { useState } from "react";
-import { Layout, Steps } from "antd";
+import  {ComponentType, useState} from "react";
+import {Layout, Steps} from "antd";
 import "./SignupLayout.css";
-const { Content } = Layout;
-const { Step } = Steps;
+const {Content} = Layout;
+const {Step} = Steps;
 import img from "../../assets/ubaky_logo.png";
 import bottomImg from "../../assets/bottom-img.png";
 
-
 interface SignupLayoutProps {
-    steps: { title: string; content: React.ComponentType<StepProps> }[];
+    steps: { title: string; content: ComponentType<StepProps> }[];
     onFinish: (values: any) => void;
 }
 
 interface StepProps {
     onNext: (data: any) => void;
     onPrevious: () => void;
-    isFirstStep: boolean;
-    isLastStep: boolean;
     stepData: any;
 }
 
-const SignupLayout: React.FC<SignupLayoutProps> = ({ steps, onFinish }) => {
+export default function SignupLayout({steps, onFinish}: SignupLayoutProps) {
     const [currentStep, setCurrentStep] = useState(1);
     const [formData, setFormData] = useState({});
 
     const handleNext = (stepData: any) => {
-        setFormData({ ...formData, ...stepData });
+        setFormData({...formData, ...stepData});
         if (currentStep === steps.length - 1) {
-            onFinish({ ...formData, ...stepData });
+            onFinish({...formData, ...stepData});
         } else {
             setCurrentStep(currentStep + 1);
         }
@@ -40,37 +37,33 @@ const SignupLayout: React.FC<SignupLayoutProps> = ({ steps, onFinish }) => {
     const CurrentStepComponent = steps[currentStep].content;
 
     return (
-        <Layout className=" restarurant-container">
+        <div className="restarurant-container">
             <aside className="left-side">
                 <div className="left-side-content">
-                    <img src={img} alt="" />
+                    <img className={"logo"} src={img} alt="ubaky-logo"/>
                     <div>
                         <Steps
+                            direction={"vertical"}
                             className={"custom-steps"}
-                            direction="vertical"
                             current={currentStep}
                         >
                             {steps.map((item, index) => (
-                                <Step key={index} title={item.title} />
+                                <Step key={index} title={item.title}/>
                             ))}
                         </Steps>
                     </div>
                 </div>
                 <div className="bottom-img">
-                    <img src={bottomImg} alt="" />
+                    <img src={bottomImg} alt=""/>
                 </div>
             </aside>
-            <Content className="outlet-details">
+            <Content className="outlet-details ">
                 <CurrentStepComponent
                     onNext={handleNext}
                     onPrevious={handlePrevious}
-                    isFirstStep={currentStep === 0}
-                    isLastStep={currentStep === steps.length - 1}
                     stepData={formData}
                 />
             </Content>
-        </Layout>
+        </div>
     );
-};
-
-export default SignupLayout;
+}
