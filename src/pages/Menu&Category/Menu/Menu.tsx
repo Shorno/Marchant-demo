f7d06707d1932850ae7cbcbc4ebe585d4042c280
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2024-12-04 09:29:29 +0600

    add menu

--- src/pages/Menu&Category/Menu/Menu.tsx
+++ src/pages/Menu&Category/Menu/Menu.tsx
@@ -1,7 +1,50 @@
+import React, { useState } from "react";
+import { Button, Card, Input, Modal } from "antd";
+import "./menu.css";
+
 export default function Menu() {
+    const [open, setOpen] = useState(false);
+
+    const showModal = () => {
+        setOpen(true);
+    };
+
+    const handleOk = () => {
+        // setLoading(true);
+        setTimeout(() => {
+            // setLoading(false);
+            setOpen(false);
+        }, 1000);
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+    };
     return (
-        <>
-            <h1>Hello, this is Menu page</h1>
-        </>
-    )
+        <div>
+            <Button className="add-menu-button " onClick={showModal}>
+                Add new menu
+            </Button>
+            <Modal
+                open={open}
+                title="Add new menu"
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={null}
+                className="custom-modal"
+                closeIcon={<span className="">×</span>}
+            >
+                <h4>Description</h4>
+                <Card style={{ width: "100%" }}>
+                    <div>
+                        <label className="label">Menu Name</label>
+                        <Input placeholder="" />
+                    </div>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+                <button className="add-menu-button">Add Product</button>
+            </Modal>
+        </div>
+    );
 }
