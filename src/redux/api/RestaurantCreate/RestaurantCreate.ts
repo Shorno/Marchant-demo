8ba53065fbdfbcf0de4c13264c4ba19f15c16d3e
rijwan sakib <71920497+rijwansakib@users.noreply.github.com>
2024-12-17 20:35:40 +0600

    info

--- src/redux/api/RestaurantCreate/RestaurantCreate.ts
+++ src/redux/api/RestaurantCreate/RestaurantCreate.ts
@@ -2,20 +2,38 @@
 import { tagTypes } from "../../tag-types";
 import { baseApi } from "../baseApi";
 
-const RESTAURSNT_CREATE = "/v2/restaurants/manager/restaurant/create/";
-
+const RESTAURSNT = "/v2/restaurants/manager/restaurant/create/";
 
 export const restaurantCreateApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+
     createRestaurant: build.mutation({
-      query: (data:any) => ({
-        url: RESTAURSNT_CREATE,
+      query: (data: any) => ({
+        url: RESTAURSNT,
         method: "POST",
         data: data,
       }),
       invalidatesTags: [tagTypes.createRestaurant],
     }),
+
+    updateRestaurant: build.mutation({
+      query: ({id,data}:any) => ({
+        url: `${RESTAURSNT}${id}/`,
+        method: "PUT",
+        data: data,
+      }),
+      invalidatesTags: [tagTypes.updateRestaurant],
+    }),
+
+    getSingleRestaurant: build.query({
+      query: ({ slug }: any) => ({
+        url: `${RESTAURSNT}${slug}/`,
+        method: "GET",
+        invalidatesTags: [tagTypes.restaurant],
+      }),
+    }),
+
   }),
 });
 
-export const { useCreateRestaurantMutation } = restaurantCreateApi;
+export const { useCreateRestaurantMutation ,useUpdateRestaurantMutation,useGetSingleRestaurantQuery} = restaurantCreateApi;
